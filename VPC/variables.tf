97b673d975117723b58db67d1ede980d718436eb
“Oanh <oanhngo9@yahoo.com>
2024-03-17 22:07:08 +0000

    remove default

---
+++ VPC/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_name" {
+  description = "The name of the VPC network"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "region" {
+  description = "The region of the project"
+  type        = string
+}
