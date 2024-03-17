5bae2e55ad6403b604262e4829d04d44a6fc77bc
“Oanh <oanhngo9@yahoo.com>
2024-03-17 21:17:36 +0000

    fixed error

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "vpc" {
+resource "google_compute_network" "dec_vpc_name" {
   name                    = var.dec_vpc_name
   auto_create_subnetworks = "true"
   routing_mode            = "GLOBAL"
