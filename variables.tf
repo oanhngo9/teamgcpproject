cfb0b91e9d95b870ad562905c56481e08970b288
“Oanh <oanhngo9@yahoo.com>
2024-03-17 21:07:21 +0000

    addec vpc

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "dec_vpc_name" {
+  type        = string
+  default     = ""
+  description = "desired name of the vpc being created"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "YourProjectID" 
+  description = "enter your project name"
+}
+
+variable "region" {
+  description = "provide a region for your code"
+  type        = string
+  default     = ""
+}
