9ca915dc53d53b605df355996293bee38b4c74b9
Alexandr Kravtsov <kravcov4k@gmail.com>
2024-03-19 07:01:11 +0000

    updated apache

--- PROJECT/apache.sh
+++ PROJECT/apache.sh
@@ -1,21 +1,64 @@
 #!/bin/bash
 set -e  # Stop the script if any command fails
+
+# Define database connection information as environment variables
+export DB_NAME="sasha-instance"
+export DB_USER="sasha-username"
+export DB_PASSWORD="12345678"
+export DB_HOST="sasha-host"
+
+# Set non-interactive frontend for apt-get
+export DEBIAN_FRONTEND=noninteractive
+
+# Update package repositories and install required packages
 sudo apt-get update
 sudo apt-get install -y apache2 unzip wget
-sudo systemctl start apache2
-sudo systemctl enable apache2
+
+# Start Apache web server
+sudo service apache2 start
+
+# Set ServerName directive
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/servername.conf
+sudo a2enconf servername
+
+# Restart Apache to apply changes
+sudo service apache2 restart
+
+# Download and install PHP and related extensions
+sudo apt-get install -y php7.3 php7.3-mysql php-pear
+
+# Download and install the protobuf compiler and PHP protobuf extension
+sudo apt-get install -y protobuf-compiler php-protobuf
+
+# Restart Apache to load new PHP extensions
+sudo service apache2 restart
+
+# Verify PHP version
+php --version
+
+# Download and extract WordPress
 sudo rm -rf /var/www/html/*
 cd /tmp
-wget https://wordpress.org/latest.zip
-unzip latest.zip
+wget https://wordpress.org/latest.tar.gz
+tar -zxvf latest.tar.gz
 sudo mv wordpress/* /var/www/html/
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php7.3 php7.3-mysql
-sudo systemctl restart apache2
-php --version
+
+# Change ownership of WordPress files to Apache user
 sudo chown -R www-data:www-data /var/www/html
+
+# Cleanup unnecessary packages
+sudo apt autoremove -y
+
+# Remove wp-config.php file if it exists
 sudo rm -f /var/www/html/wp-config.php
-cat <<EOF | sudo tee /var/www/html/wp-config.php
+
+# Create a new wp-config.php file
+sudo tee /var/www/html/wp-config.php > /dev/null << EOF
+<?php
+define('DB_NAME', '$DB_NAME');
+define('DB_USER', '$DB_USER');
+define('DB_PASSWORD', '$DB_PASSWORD');
+define('DB_HOST', '$DB_HOST');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
 EOF
--- PROJECT/variables.tf
+++ PROJECT/variables.tf
@@ -72,42 +72,42 @@ variable "data_base_version" {
 
 variable "dbinstance_name" {
   type        = string
-  default = "sasha-instance"
+  default     = "sasha-instance"
   description = "Name of the database instance"
 }
 
 variable "db_version" {
   type        = string
-  default = "MYSQL_5_7"
+  default     = "MYSQL_5_7"
   description = "Version of the database"
 }
 
 variable "db_region" {
   type        = string
-  default = "us-central1"
+  default     = "us-central1"
   description = "Region where the resources will be created"
 }
 
 variable "db_username" {
   type        = string
-  default = "sasha-username"
+  default     = "sasha-username"
   description = "Username for the database"
 }
 
 variable "db_host" {
   type        = string
-  default = "sasha-host"
+  default     = "sasha-host"
   description = "Host for the database"
 }
 
 variable "db_password" {
   type        = string
-  default = "12345678"
+  default     = "12345678"
   description = "Password for the database"
 }
 
 variable "db_name" {
   type        = string
-  default = "sasha-db"
+  default     = "sasha-db"
   description = "Name of the database"
 }
